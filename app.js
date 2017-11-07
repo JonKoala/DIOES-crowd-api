6dab204b4839fa71aae5be2ff4efd028c1dd4575
JonKoala <victor.santana.lemos@gmail.com>
2017-11-07 17:15:08 -0200

    basic server up and running

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var appconfig = require('./appconfig')
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var port = appconfig['server']['port'];
+app.listen(port, function() {
+  console.log('Server up and running! Listening on ' + port + '...');
+});
