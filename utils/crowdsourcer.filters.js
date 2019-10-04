175ec3ba11c752dd570154703878fcb6a7afb27c
JonKoala <victor.santana.lemos@gmail.com>
2019-10-04 19:20:17 -0300

    adiciona tipos de publicacao do diario da amunes

--- utils/crowdsourcer.filters.js
+++ utils/crowdsourcer.filters.js
@@ -1,4 +1,8 @@
+
+tiposIoes = ['Aviso de Licitação', 'Contrato', 'Aditivo', 'Ata Registro de Preço', 'Inexigibilidade de Licitação', 'Dispensa de Licitação', 'Portaria', 'Edital', 'Convocação']
+tiposAmunes = ['Ata de registro de preços', 'Atas', 'Concursos', 'Contratos', 'Convênios', 'Decretos', 'Editais de Notificação', 'Leis', 'Licitações', 'Outras publicações', 'Portarias', 'Resoluções']
+
 module.exports = {
-  tipos: ['Aviso de Licitação', 'Contrato', 'Aditivo', 'Ata Registro de Preço', 'Inexigibilidade de Licitação', 'Dispensa de Licitação', 'Portaria', 'Edital', 'Convocação'],
+  tipos: tiposIoes.concat(tiposAmunes),
   classes: ['SAÚDE E ASSISTÊNCIA SOCIAL', 'EDUCAÇÃO E SEGURANÇA PÚBLICA', 'ENGENHARIA E MEIO AMBIENTE', 'TECNOLOGIA DA INFORMAÇÃO', 'GERAL', 'PREVIDÊNCIA E PESSOAL']
 }
